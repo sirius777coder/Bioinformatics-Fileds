e653bbb6ac7dd9b601393755dd5acc47dfc31831
Bo Zhang <bozhang@f130.hn.org>
2024-01-08 01:13:54 +0800

    update namd scripts

---
+++ simulation/03_openmm.py
@@ -0,0 +1,17 @@
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+
+pdb = PDBFile('input.pdb')
+forcefield = ForceField('amber14-all.xml', 'amber14/tip3pfb.xml')
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=NoCutoff,
+        nonbondedCutoff=1*nanometer, constraints=HBonds)
+integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.004*picoseconds)
+simulation = Simulation(pdb.topology, system, integrator)
+simulation.context.setPositions(pdb.positions)
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+        potentialEnergy=True, temperature=True))
+simulation.step(10000)
